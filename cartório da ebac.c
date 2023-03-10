c5d86794c9fb08e3ae97529ef6a21e32a7fe623d
Richard Freitas <123339564+RichardFFreitas@users.noreply.github.com>
2023-03-10 18:02:08 -0300

    update do login

--- cartório da ebac.c
+++ cartório da ebac.c
@@ -123,16 +123,20 @@ int main()
 {
 	int opcao = 0; //Definindo variáveis
 	int x = 1;
-	char senhadigitada[10] = "a";
+	char senhadigitada[] = "a";
+	int comparacao;
+	
 	
 	printf("### Cartório da EBAC ### \n\n");
 	printf("Login de administrador!\n\nDigite a sua senha: ");
-	scanf("%S", senhadigitada);
+	scanf("%s", senhadigitada);
 	
-	if(senhadigitada[10] == "admin")
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0)
 	{
 		
-	
+			system("cls");
 	
 	for(x = 1;x = 1;)
 		{

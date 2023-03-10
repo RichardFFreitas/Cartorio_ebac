1ca0dfa563e4da6c0b027e36f302d37d21dc0083
Richard Freitas <123339564+RichardFFreitas@users.noreply.github.com>
2023-03-10 17:50:16 -0300

    Senha de acesso admin

--- cartório da ebac.c
+++ cartório da ebac.c
@@ -123,61 +123,73 @@ int main()
 {
 	int opcao = 0; //Definindo variáveis
 	int x = 1;
+	char senhadigitada[10] = "a";
+	
+	printf("### Cartório da EBAC ### \n\n");
+	printf("Login de administrador!\n\nDigite a sua senha: ");
+	scanf("%S", senhadigitada);
+	
+	if(senhadigitada[10] == "admin")
+	{
+		
+	
 	
 	for(x = 1;x = 1;)
-	{
+		{
 	
 	
-		system("cls"); //cls é responsável para resetar a tela do sistema
+			system("cls"); //cls é responsável para resetar a tela do sistema
 	
 	
 	// Definindo a linguagem	
-		setlocale(LC_ALL, "Portuguese");
+			setlocale(LC_ALL, "Portuguese");
 	
-		printf("### Cartório da EBAC ### \n\n");
+			printf("### Cartório da EBAC ### \n\n");
 	// Inicio do menu
-		printf("Escolha a opção desejada do menu: \n\n");
+			printf("Escolha a opção desejada do menu: \n\n");
 	// Escolhas do usuario
-		printf("\t1 - Registrar nomes\n");
-		printf("\t2 - Consultar os nomes\n");
-		printf("\t3 - Deletar nomes\n");
-		printf("\t4 - Sair do Sistema\n");
-		printf("Opção: ");
+			printf("\t1 - Registrar nomes\n");
+			printf("\t2 - Consultar os nomes\n");
+			printf("\t3 - Deletar nomes\n");
+			printf("\t4 - Sair do Sistema\n");
+			printf("Opção: ");
 	
-		scanf("%d", &opcao); //armazenando a escolha do usuario
+			scanf("%d", &opcao); //armazenando a escolha do usuario
 		
-		system("cls");
+			system("cls");
 		
 		
-			switch(opcao)
-		{
-			case 1:
-			registro();
-			break;
+				switch(opcao)
+			{
+				case 1:
+				registro();
+				break;
 				
-			case 2:
-			consulta();
-			break;
+				case 2:
+				consulta();
+				break;
 				
-			case 3:
-			deletar();	
-			break;
+				case 3:
+				deletar();	
+				break;
 			
-			case 4:
-			printf("Obrigado por usar o sistema!\n");
-			return 0;
-			break;
+				case 4:
+				printf("Obrigado por usar o sistema!\n");
+				return 0;
+				break;
 			
 				
-			default:
+				default:
 				printf("Está opção não está disponivel\n");
 				system("pause");
-			break;
-		}
+				break;
+			}
 		//fim da seleção
-		
+		}
 	//	printf("\n\nEsse Software é Pertencente à Richard Felipe");
 	}
+	else
+	printf("Senha incorreta!");
 }
 
 
